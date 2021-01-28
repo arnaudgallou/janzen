ebdfbd99a732bce376e04b15d6e5d6748ce40837
arnaudgallou <67476099+arnaudgallou@users.noreply.github.com>
2021-01-28 15:38:49 +0100

    commit shiny

---
+++ shiny/r/app.R
@@ -0,0 +1,31 @@
+####  Init  ####
+  {
+    # · Librairies ----
+      {
+        library(shiny)
+        library(tidyverse)
+        library(ggrepel)
+      }
+    
+    # · Data ----
+      {
+        shiny_bioclim <- read_csv("shiny/data/shiny_bioclim.csv")
+        shiny_paleoclim <- read_csv("shiny/data/shiny_paleoclim.csv")
+        shiny_janzen <- read_csv("shiny/data/shiny_janzen.csv")
+      }
+    
+    # · Function ----
+      {
+        floor_nearest <- function(x, nearest = 1000) {
+          (x %/% nearest) * nearest
+        }
+      }
+  }
+
+####  App  ####
+  {
+    source("shiny/r/ui.R")
+    source("shiny/r/server.R")
+    
+    shinyApp(ui = ui, server = server)
+  }
